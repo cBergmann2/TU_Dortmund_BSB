c55e74d7cccbf7a1a17c34288a9794c861eab832
Benedikt Ruthenberg <benedikt.ruthenberg@googlemail.com>
2014-10-26 10:49:14 +0100

    Testprogramm erstellt

--- Aufgabe_1/vorgabe1/main.cc
+++ Aufgabe_1/vorgabe1/main.cc
@@ -2,20 +2,58 @@
 
 /* Hier muesst ihr selbst Code vervollstaendigen */ 
 #include "device/cgastr.h"
+#include "maschine/keyctrl.h"
 
+CGA_Stream kout;
 
 int main()
 {
-/* Hier muesst ihr selbst Code vervollstaendigen */         
-         
-/* Hier muesst ihr selbst Code vervollstaendigen */ 
-                         
-/* Hier muesst ihr selbst Code vervollstaendigen */                         
- 
-	CGA_Stream t;
+	//Initialisierungen
+	unsigned int fehler=0;
+	Keyboard_Controller kc;
+	Key input;
+	unsigned char inbuffer[11] = {0};
+	unsigned char index=0;
+	//Bildshirm initialisieren
+	kout << "Guten Tag,\n das ist OOSTUBS, ein kleines OS von Stundenten für Studenten";
+	
+	
 
-	t << "hallowoslsowikskskdjkfdjgfkflkfosksdölkjasölkdlaöshdlöhd";
-	t.flush();
+	do
+	{
+		kout << endl << "Geben sie hier eine Zeichenkette ein: ";
+		
+		//Eingabe abfragen
+		index=0;
+		do
+		{
+			input = kc.key_hit();
+			if(input.ascii() == '\n')
+			{
+				inbuffer[index] = 0;
+				break;
+			}else{
+				inbuffer[index] = input.ascii();
+				index++;
+				if(index==10)
+				{
+					fehler = 1;
+					goto ERROR;
+				}
+			}	
+		}while(1);
+		
+		//Zahl ausgeben
+		kout << endl << "Sie haben folgende Zeichenkette eingeben: ";
+		kout << inbuffer;
+		kout << endl;
+	} while (1);
+   
+ERROR:
+	//Error-handler:
+	kout << endl << endl << "Hier ist ein Fehler aufgetreten." << endl;
+	kout << "Fehlernummer "  << fehler;
+	while(1);
    
    return 0;
  }
