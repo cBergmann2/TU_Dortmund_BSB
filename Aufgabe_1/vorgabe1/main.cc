20345e721a0a502f37085af3834bac6f1062100d
stud <stud@debian>
2014-10-28 08:36:16 +0100

    Ueberarbeitung main mit mehreren Testfaellen

--- Aufgabe_1/vorgabe1/main.cc
+++ Aufgabe_1/vorgabe1/main.cc
@@ -7,6 +7,8 @@
 #define BUFFERSIZE 100
 CGA_Stream kout;
 
+void decrementPos();
+
 int main()
 {
 	//Initialisierungen
@@ -25,6 +27,13 @@ int main()
 	kout << "Guten Tag,\n das ist OOSTUBS, ein kleines OS von Stundenten für Studenten";
 	kout.flush();
 
+	kout << "\nJetzt folgen zunächst ein paar Testausgaben:\n";
+	kout << "int 25500: " << 25500 << " hex: " << hex << 25500 << " oct: " << oct << 25500;
+	kout << endl;
+	kout << "char 2500: " << (char)25500 << " hex: " << hex << (char)25500 << " oct: " << oct << (char)25500;
+	kout << endl;
+	kout << "Zeiger-Typ: " << &index;
+	
 	do
 	{
 		kout << endl << "Geben sie hier eine Zeichenkette ein (max. 10 Zeichen): ";
@@ -43,7 +52,12 @@ int main()
 				input = kc.key_hit();
 			}while(!input.valid());
 			zeichen = input.ascii();
-			kout.print(&zeichen, 1);
+			if(zeichen == '\b')
+			{
+			      decrementPos();
+			}else{
+			      kout.print(&zeichen, 1);
+			}
 			
 			if(zeichen == '\n')
 			{
@@ -76,4 +90,23 @@ ERROR:
 	while(1);
    
    return 0;
- }
+}
+
+ 
+void decrementPos()
+{
+    int zeile, spalte;
+    
+    kout.getPos(spalte,zeile);
+    
+    if(spalte==0)
+    {
+	spalte = 79;
+	zeile--;
+    }else{
+	spalte--;
+    }
+    
+    kout.setPos(spalte, zeile);
+  
+}
