6f6d836f0cef84c0ee3c88290101695a852b0bbb
stud <stud@debian>
2014-11-04 16:51:57 +0100

    kout in main, testprogramm

--- Aufgabe_1/vorgabe1/user/appl.cc
+++ Aufgabe_1/vorgabe1/user/appl.cc
@@ -20,7 +20,23 @@ extern CGA_Stream kout;
 /* Hier muesst ihr selbst Code vervollstaendigen */ 
          
 void Application::action ()
- {
+{
 /* Hier muesst ihr selbst Code vervollstaendigen */ 
- 
- }
+
+  int i=0;
+  bool b=true;
+  while (1)
+  {
+      i++;
+      if(i>0xF000)
+      {
+	i=0;
+	if(b)
+	  kout.show(0,0,' ');
+	else
+	  kout.show(0,0,'*');
+	b=!b;
+      }
+  }
+
+}
