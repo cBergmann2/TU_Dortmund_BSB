793b20ced312742de7b32f57132351a3dd504bab
Christoph Bergmann <c.bergmann2@gmx.net>
2015-01-19 14:00:35 +0100

    Erweiterung und Implementierung der Keyboard Klassen Ein Tastendruck wird nun nicht mehr direkt ausgegeben, sondern in eim Ringpuffer gespeichert. Inhalte des Ringbuffers können über die Funktion getkey() abgerufen werden. getKey() ist ein blockierender Aufruf, sofern keine Zeichen im Puffer vorhanden sind.

--- Aufgabe_1/vorgabe1/device/keyboard.cc
+++ Aufgabe_1/vorgabe1/device/keyboard.cc
@@ -16,6 +16,7 @@
 extern Plugbox plugbox;
 extern CGA_Stream kout;
 extern Guard guard;
+extern Semaphore keyboardsem;
 
  
 Keyboard::Keyboard() :
@@ -30,6 +31,11 @@ void Keyboard::plugin()
 
 	plugbox.assign(plugbox.keyboard, *this);
 	pic.allow(keyboard); //1 - Keyboard, 0 - timer
+
+	//Zeichenpuffer vorbereiten
+	this->belegungsgrad = 0;
+	this->bufferHead = 0;
+	this->bufferTail = 0;
 
 	return;
 }
@@ -70,14 +76,16 @@ bool Keyboard::prologue ()
 			{
 				//Für Ausgabe vorbereiten
 				//Theoretisch in Ringbuffer speichern, 
-				//damit weitere Zeichen ausgelesen werden können
-				if(this->zeichen!=0) 
+				//damit weitere Zeichen ausgelesen werden können				
+				
+				if (this->key.ascii() != 0)
 				{
 					//bereits ein Zeichen im buffer.
 					return false;
 				}
-				this->zeichen = zeichen;
-				
+				this->key = intput;
+				//this->zeichen = zeichen;
+
 				//Epilogue anfordern
 				return true;
 			}
@@ -92,13 +100,61 @@ bool Keyboard::prologue ()
     
 
 /**
-  * Hier wird das im Rahmen der Prolog-Behandlung ausgelesene Zeichen auf dem 
-  * Bildschirm mit Hilfe des globalen CGA_Stream Objekts kout ausgegeben.
+  * In dieser Methode soll das Auftreten des Tastendrucks einem eventuell wartenden Prozess signalisiert werden.
+  * Dazu wird ein Semaphor verwendet, dessen Zähler angibt,
+  * wieviele Tastaturcodes im Keyboard Puffer zur Abholung bereitstehen.
+  * Wenn ein Puffereintrag überschrieben werden muss, ohne dass der alte Wert von einem Anwendungsprozess abgefragt wurde,
+  * darf der Semaphor also nicht hochgezählt werden.
   */
 void Keyboard::epilogue ()
 {
+	if (this->addKey2Buffer(this->key)){
+		kout << "Tasterturpuffer voll";
+	}
+	else{
+		//Semaphore hochzählen
+		this->keySem.v();
+		//Bufferzustand auf leer setzen
+		this->key.ascii(0);
+	}
+
+	/*
 	kout << zeichen;
 	
 	//Buffer zurücksetzen
 	zeichen = 0;
+	*/
+}
+
+/**
+* Diese Methode liefert die zuletzt gedrückte Taste (als Objekt der Klasse Key) zurück.
+* Wenn gerade keine Taste gedrückt wurde, wird der aufrufende Anwendungsprozess solange blockiert.
+* Dies wird durch die Verwendung eines Semaphors erreicht. Sobald der Tastencode abgeholt worden ist,
+* sollte geeignet vermerkt werden, dass der verwendete Puffer nun für den nächsten Tastencode zur Verfügung steht.
+*/
+Key Keyboard::getkey(){
+	
+	this->keySem.p();
+	return this->removeKeyFromBuffer();
+}
+
+
+int addKey2Buffer(Key zeichen){
+	if (this->belegungsgrad < CHAR_BUFFER_LENGTH){
+		this->buffer[this->bufferTail] = zeichen;
+		this->bufferTail = (bufferTail + 1) % CHAR_BUFFER_LENGTH;
+		this->belegungsgrad++;
+
+		return 0; //Zeichen konnte erfolgreich in Buffer eingefügt werden
+	}
+	return -1; //Buffer ist bereits voll
+}
+
+Key removeKeyFromBuffer(){
+	Key tmp;
+
+	tmp = this->buffer[this->bufferHead];
+	this->belegungsgrad--;
+	this->bufferHead = (this->bufferHead + 1) % CHAR_BUFFER_LENGTH;
+	return tmp;
 }
